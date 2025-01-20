66d749dc0a5970358949c57c8a4910e04757fc14
arya-bhanu <aryacreative.dev@gmail.com>
2025-01-20 07:56:56 +0700

    init

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,31 @@
 package org.example;
 
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Solution sol = new Solution();
+        int[] arr1 = new int[]{0,0,1,1,1,2,2,3,3,4};
+        int[] arr2 = new int[]{1,1,2};
+        int length = sol.removeDuplicates(arr1);
+        int length2 = sol.removeDuplicates(arr2);
+        System.out.println(length);
+        System.out.println(length2);
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(arr2));
+    }
+}
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int k = 1;
+        for(int i = k; i < nums.length; i++){
+           if(nums[i] != nums[i-1]){
+               nums[k] = nums[i];
+               ++k;
+           }
+       }
+        return k;
     }
 }
