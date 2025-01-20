77620ef41b1107e8f11a2f38accc9d631c5f993d
arya-bhanu <aryacreative.dev@gmail.com>
2025-01-20 08:48:39 +0700

    solved

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int [] arr1 = new int[] {0,1,2,2,3,0,4,2};
+        int val1 = 2;
+        System.out.println(new Solution().removeElement(arr1,val1));
+        System.out.println(Arrays.toString(arr1));
+    }
+}
+
+class Solution{
+    public int removeElement (int [] arr, int val){
+        int k = 0;
+        for(int i = k; i < arr.length; i++){
+            if(arr[i] != val){
+                arr[k] = arr[i];
+                ++k;
+            }
+        }
+        return k;
     }
 }
