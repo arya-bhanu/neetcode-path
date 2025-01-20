81c61338fbcc2e08f0906a41c45dcb4028ab6e3e
arya-bhanu <aryacreative.dev@gmail.com>
2025-01-20 08:59:07 +0700

    solved

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int[] arr1 = new int[] {0,1,0,3,12};
+        moveZeroes(arr1);
+        System.out.println(Arrays.toString(arr1));
+    }
+    public static void moveZeroes(int [] nums){
+        if(nums.length == 1){
+            return;
+        }
+        int k = 0;
+        for(int i = k; i < nums.length; i++){
+            if(nums[i] != 0){
+                nums[k] = nums[i];
+                ++k;
+            }
+        }
+        for(int j = k; j < nums.length; j++){
+            nums[j] = 0;
+        }
     }
 }
