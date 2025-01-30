1ac130cf0e4000bd071c73cf39b1ead1f0bd9d1b
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-01-30 16:13:18 +0700

    solved with own solution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,32 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        String haystack = "sadbutsad";
+        String needle = "sad";
+        String haystack2 = "leetcode";
+        String needle2 = "leeto";
+        String haystack3 = "hello";
+        String needle3 = "ll";
+        System.out.println(strStr(haystack, needle));
+        System.out.println(strStr(haystack2, needle2));
+        System.out.println(strStr(haystack3, needle3));
+    }
+
+// Efficient Solution, My Own Solution, 100% beats the others
+    public static int strStr(String haystack, String needle) {
+        int lengthHay = haystack.length();
+        int lengthNeed = needle.length();
+        int result = -1;
+        for(int i = 0; i <= lengthHay - lengthNeed; i++){
+            for (int j = 0; j < lengthNeed;j++){
+                if(needle.charAt(j) != haystack.charAt(j + i)){
+                    break;
+                }
+                if(j == lengthNeed - 1){
+                    return i;
+                }
+            }
+        }
+        return result;
     }
 }
