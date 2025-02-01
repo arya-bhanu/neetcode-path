b8f778a83b198a6459ebc93a609eb57ffa0d8754
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-02-01 13:38:42 +0700

    solved with my own ways

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    public static int isPrefixOfWord(String sentence, String searchWord) {
+//        My own solution
+        String [] words = sentence.split("\\s+");
+        for (int i = 0; i < words.length; i++){
+            if(words[i].startsWith(searchWord)) return i + 1;
+        }
+        return -1;
+    }
 }
