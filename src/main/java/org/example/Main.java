e33334ab0da33a91613fa277e1033428f26aa95a
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-02-02 12:50:35 +0700

    solved with my own solution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,37 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int [] ex1 = new int[] {1,2,3};
+        int [] ex2 = new int[] {9};
+        int [] ex3 = new int[] {9,8,7,6,5,4,3,2,1,0};
+        System.out.println(Arrays.toString(plusOne(ex1)));
+        System.out.println(Arrays.toString(plusOne(ex2)));
+        System.out.println(Arrays.toString(plusOne(ex3)));
+    }
+//    do not use copy array into another memory array. we can create a new size from same memory
+//    algorithm below seems complex
+    public static int[] plusOne(int[] digits) {
+        int lastDigit = digits[digits.length -1];
+        if(lastDigit != 9){
+            digits[digits.length - 1] = ++lastDigit;
+            return digits;
+        }
+        for(int i = digits.length - 1; i >= 0; i--){
+            digits[i] = 0;
+            if ((i - 1 )>= 0){
+                int prevVal = digits[i-1];
+                if(prevVal != 9){
+                    digits[i-1] = ++prevVal;
+                    return digits;
+                }
+            }
+        }
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
     }
 }
