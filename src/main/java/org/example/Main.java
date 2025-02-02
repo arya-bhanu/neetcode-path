e6188a267525754689b85f834a366890e9130afc
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-02-02 21:48:30 +0700

    not solved -(salah pemahaman)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,25 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(maxRepeating("ababc","ab"));
+        System.out.println(maxRepeating("ababc","ba"));
+        System.out.println(maxRepeating("ababc","ac"));
+    }
+    public static int maxRepeating(String sequence, String word) {
+        if(!sequence.contains(word)) return 0;
+        int targetLength = word.length();
+        char fc = word.charAt(0);
+        int counter = 0;
+        int i = 0;
+        int j = i + targetLength - 1;
+        while (j < sequence.length()){
+            if(sequence.charAt(i) == fc){
+                String str = sequence.substring(i, j + 1);
+                if(str.equals(word)) counter++;
+            }
+            ++i;
+            j = i + targetLength - 1;
+        }
+        return counter;
     }
 }
