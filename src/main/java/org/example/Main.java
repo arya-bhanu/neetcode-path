a3cec6a91ba4d681602b0d60e246650704603677
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-02-03 08:32:50 +0700

    problem solved with my own solution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,19 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        String [] words1 = {"pay","attention","practice","attend"};
+        String [] words2 = {"leetcode","win","loops","success"};
+        String pref1 = "at";
+        String pref2 = "code";
+        System.out.println(prefixCount(words1, pref1));
+        System.out.println(prefixCount(words2, pref2));
+    }
+    public static int prefixCount(String[] words, String pref) {
+        int counter = 0;
+        for(String str: words){
+            if(str.length() < pref.length()) continue;
+            if(str.startsWith(pref)) counter++;
+        }
+        return counter;
     }
 }
