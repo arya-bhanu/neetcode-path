68f1910ec44e66be6a8821959c10f5a339a18885
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-02-03 21:56:20 +0700

    problem solved with (neetcode solution)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,34 @@
 package org.example;
 
+import java.util.HashSet;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+//        System.out.println(isHappy(3));
+        System.out.println(sumOfSquares(81));
+    }
+
+    public static boolean isHappy(int n) {
+        HashSet<Integer> hs = new HashSet<>();
+        int val = n;
+        while (!hs.contains(val)){
+            hs.add(val);
+            val = sumOfSquares(val);
+            System.out.println(hs);
+            System.out.println(val);
+            if(val == 1) return true;
+        }
+        System.out.println(hs);
+        return false;
+    }
+    public static int sumOfSquares(int n){
+        int value = 0;
+        while(n != 0){
+            int sqFirst = (int) Math.pow(n % 10,2);
+            n = n / 10;
+            value += sqFirst;
+        }
+        return value;
     }
 }
