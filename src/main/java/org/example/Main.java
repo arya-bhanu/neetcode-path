26ac2a365d99a3a3fdf520e2064294dd41d874da
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-02-03 20:06:05 +0700

    problem solved with my own solution -(neetcode logic)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,36 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        String s1 = "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth";
+        String s2 = "ccc";
+        String s3 = "bananas";
+        System.out.println(longestPalindrome(s1));
+//        System.out.println(longestPalindrome(s2));
+//        System.out.println(longestPalindrome(s3));
+    }
+    public static int longestPalindrome(String s) {
+        Map<Character,Integer> map = new HashMap<>();
+        boolean hasOdd = false;
+        int counter = 0;
+        for(Character c: s.toCharArray()){
+            map.put(c, map.getOrDefault(c,0) + 1);
+        }
+        for(var mapVal : map.entrySet()){
+            if(mapVal.getValue() % 2 == 0){
+                counter += mapVal.getValue();
+                continue;
+            }
+            hasOdd = true;
+            if(mapVal.getValue() > 2){
+                counter += mapVal.getValue() - 1;
+            }
+        }
+        if(hasOdd) counter++;
+        return counter;
     }
 }
