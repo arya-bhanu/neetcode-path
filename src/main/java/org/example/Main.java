ed129d4ed3d94af377b3739a9c77eee9752b9a26
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-02-03 20:25:54 +0700

    problem solved with my own solution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int [] arr1 = {1,2,3,1};
+        int [] arr2 = {1,2,3,4};
+        int [] arr3 = {1,1,1,3,3,4,3,2,4,2};
+        System.out.println(containsDuplicate(arr1));
+        System.out.println(containsDuplicate(arr2));
+        System.out.println(containsDuplicate(arr3));
+    }
+//    use hashmap
+    public static boolean containsDuplicate(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(Integer i :nums){
+            int val = map.getOrDefault(i,1);
+            if(val > 1) return true;
+            map.put(i, ++val);
+        }
+        return false;
     }
 }
