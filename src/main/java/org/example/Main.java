63a4d16979311e63cf9759e6f30e5f0250f0c544
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-02-03 08:40:33 +0700

    problem solved with my own solution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,18 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String [] words = {"a","b","c","ab","bc","abc"};
+        String s = "abc";
+        String [] words2 = {"a","a"};
+        String s2 = "aa";
+        System.out.println(countPrefixes(words,s));
+        System.out.println(countPrefixes(words2,s2));
+    }
+    public static int countPrefixes(String[] words, String s) {
+        int counter = 0;
+        for(String str: words){
+            if(s.startsWith(str)) counter++;
+        }
+        return counter;
     }
 }
