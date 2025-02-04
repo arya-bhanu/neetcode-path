0f7778e0d1c503f8674ba6e7ae126ea0266f9943
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-02-04 11:42:33 +0700

    problem solved with naive approach

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(missingNumber(new int[] {3,0,1}));
+        System.out.println(missingNumber(new int[] {0,1}));
+        System.out.println(missingNumber(new int[] {1,2,3,5,0}));
+    }
+//    naive approach
+    public static int missingNumber(int[] nums) {
+        int range = nums.length + 1;
+        Arrays.sort(nums);
+        for(int i = 0; i < range; i++){
+            if(nums.length <= i) return nums[nums.length - 1] + 1;
+            if(nums[i] != i) return i;
+        }
+        return -1;
     }
 }
