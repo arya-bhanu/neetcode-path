c099b343f86d16ec34b2e2fb78d4bcf660611a03
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-02-11 07:17:02 +0700

    solved with my own solution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,31 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(canConstruct("a","b"));
+        System.out.println(canConstruct("aa","ab"));
+        System.out.println(canConstruct("aa","aab"));
+        System.out.println(canConstruct("abxcc","characterbin"));
+    }
+    public static boolean canConstruct(String ransomNote, String magazine) {
+        Map<Character, Integer>  map = new HashMap<>();
+        for(Character c: ransomNote.toCharArray()){
+            map.put(c, map.getOrDefault(c,0) + 1);
+        }
+        for(Character c: magazine.toCharArray()){
+            if(map.containsKey(c)){
+                int val = map.get(c);
+                if(val == 1) {
+                    map.remove(c);
+                    continue;
+                }
+                --val;
+                map.put(c,val);
+            }
+        }
+        return map.isEmpty();
     }
 }
