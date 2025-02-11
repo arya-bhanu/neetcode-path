1a2fc9a0033d79cba33cd7fbccf0fb4cf53caa25
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-02-11 07:08:41 +0700

    not solved, will check later

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,44 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+//        System.out.println(maximumCount(new int[]{-2,-1,-1,1,2,3}));
+//        System.out.println(maximumCount(new int[]{-3,-2,-1,0,0,1,2}));
+        System.out.println(maximumCount(new int[]{0,0,0,0,0}));
+    }
+    public static int maximumCount(int[] nums) {
+//        search for negative number
+        int l = 0;
+        int r = nums.length - 1;
+        int neg = 0;
+        while (l <= r){
+            int m = (l + r) / 2;
+            if(nums[m] >= 0){
+                r = m - 1;
+                continue;
+            }
+            if(nums[m] < 0){
+                l = m + 1;
+                neg = m + 1;
+            }
+        }
+
+        l = neg;
+        r = nums.length - 1;
+        int pos = 0;
+        System.out.println(neg);
+//        search for positive number
+        while (l <= r){
+            int m = (l + r) / 2;
+            if(nums[m] <= 0){
+                l = m + 1;
+                continue;
+            }
+            if(nums[m] > 0){
+                pos = nums.length - m;
+                r = m -1;
+            }
+        }
+//        if(pos == 0) return 0;
+        return Math.max(neg,nums.length - pos);
     }
 }
