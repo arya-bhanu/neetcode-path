d4fc0cc9a8d8af83c01bb3cf08ddf6bf829b7d4a
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-02-15 13:22:30 +0700

    solved with others solution, do manual checking, without 3 nested loop, but still a nested loop

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,29 @@
 package org.example;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(Arrays.toString(findWords(new String[]{"Hello", "Alaska", "Dad", "Peace"})));
+    }
+//    manual checking for other 3 rows
+    public static String[] findWords(String[] words) {
+        List<String> result = new LinkedList<>();
+        String first = "qwertyuiop";
+        String second = "asdfghjkl";
+        String third = "zxcvbnm";
+        for(String s : words){
+            if(checkIsExist(s.toLowerCase(),first) || checkIsExist(s.toLowerCase(),second) || checkIsExist(s.toLowerCase(), third)){
+                result.add(s);
+            }
+        }
+        return result.toArray(new String[0]);
+    }
+    public static boolean checkIsExist(String search, String row){
+        for(Character c  : search.toCharArray()){
+            if(row.indexOf(c) == -1) return false;
+        }
+        return true;
     }
 }
