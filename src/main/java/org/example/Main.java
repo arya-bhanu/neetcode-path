8956a5267110c533f88f1f15d7978b0257e85078
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-02-15 12:19:42 +0700

    solved with my solution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(findDisappearedNumbers(new int[]{4,3,2,7,8,2,3,1}));
+        System.out.println(findDisappearedNumbers(new int[]{1,1}));
+    }
+//    naive approach
+    public static  List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
+        for(Integer i : nums){
+            set.add(i);
+        }
+        for(int i = 1; i <= nums.length; i++){
+            if(!set.contains(i)){
+                result.add(i);
+            }
+        }
+        return result;
     }
 }
