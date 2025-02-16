7ccb540ed411e54c63670590cc855a21935fdec9
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-02-16 10:58:11 +0700

    solved with own solution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(heightChecker(new int[] {1,1,4,2,1,3}));
+    }
+    public static int heightChecker(int[] heights) {
+        int counter = 0;
+        int [] heightOriginal = heights.clone();
+        Arrays.sort(heights);
+        int i = 0;
+        for(Integer j : heightOriginal){
+            if (j != heights[i]) counter++;
+            ++i;
+        }
+        return counter;
     }
 }
