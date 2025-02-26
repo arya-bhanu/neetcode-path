baa62eabfeab558dab388d2b37f1164d61f4fc17
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-02-26 23:13:32 +0700

    solved with my own solution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(Arrays.toString(getConcatenation(new int[]{1,2,1})));
+    }
+    public static int[] getConcatenation(int[] nums) {
+        int[] newArr = new int[2*nums.length];
+        int i = 0;
+        int j = nums.length;
+        for(int val: nums){
+            newArr[i] = val;
+            newArr[j] = val;
+            ++j;
+            ++i;
+        }
+        return newArr;
     }
 }
