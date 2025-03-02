688abe90ea99e3d9564bd60549cf843815b03477
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-03-02 10:19:22 +0700

    solved with my own solution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,21 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(mergeAlternately("abc","pqr"));
+        System.out.println(mergeAlternately("ab","pqrs"));
+        System.out.println(mergeAlternately("abcd","pq"));
+    }
+    public static  String mergeAlternately(String word1, String word2) {
+        StringBuilder word = new StringBuilder();
+        int max = Math.max(word1.length(), word2.length());
+        for(int i = 0; i < max; i++){
+            if(i < word1.length()){
+                word.append(word1.charAt(i));
+            }
+            if(i < word2.length()){
+                word.append(word2.charAt(i));
+            }
+        }
+        return word.toString();
     }
 }
