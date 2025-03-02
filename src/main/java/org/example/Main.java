5245c8271d2ee4e0611fd5e9a2f3496ff10baed6
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-03-02 09:56:45 +0700

    solved with my own solution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(findGCD(new int[]{2,5,6,9,10}));
+        System.out.println(findGCD(new int[]{7,5,6,8,3}));
     }
+    public static int findGCD(int[] nums) {
+        Arrays.sort(nums);
+        int min = nums[0];
+        int max = nums[nums.length - 1];
+        while (max % min != 0){
+            int temp = max % min;
+            max = min;
+            min = temp;
+        }
+        return min;
+    }
+
 }
